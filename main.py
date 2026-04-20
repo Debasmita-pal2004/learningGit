a43bec8774f9ae6359d3e8d45c79cb941ecf8708
Debasmita-pal2004 <debasmitapal18@gmail.com>
2026-04-20 12:47:35 +0530

    initial insertion

---
+++ main.py
@@ -0,0 +1,13 @@
+def sorting(arr):
+    
+    for i in range(len(arr)):
+        curr=arr[i]
+        j=i-1
+        while j>=0 and arr[j]>curr:
+            arr[j+1]=arr[j]
+            j-=1
+        arr[j+1]=curr
+    return arr  
+if __name__=="__main__":
+    arr=[12,11,13,5,6]
+    print(sorting(arr))
