a906b0c5a4046cd010e4f1692c6c79261c7b38d5
Debasmita-pal2004 <debasmitapal18@gmail.com>
2026-04-26 21:24:24 +0530

    new changes

--- one.py
+++ one.py
@@ -1,11 +1,2 @@
-def selection(arr):
-    for i in range(len(arr)):
-        min_idx=i
-        for j in range(i+1,len(arr)):
-            if arr[j]<arr[min_idx]:
-                min_idx=j
-        arr[i],arr[min_idx]=arr[min_idx],arr[i]
-    return arr
-if __name__=="__main__":
-    arr=[64,25,12,22,11]
-    print(selection(arr))
+def show():
+    print("This is a palindrome checker.")
